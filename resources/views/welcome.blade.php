cc66d4e74a117279d85761089e74aa99f62afd05
Mahmoud Badr <mahmoud.hussiba@gmail.com>
2023-08-16 01:19:53 +0300

    Done, auth setup

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Welcome</div>
+
+                    <div class="card-body">
+                        This a realtime laravel application
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
