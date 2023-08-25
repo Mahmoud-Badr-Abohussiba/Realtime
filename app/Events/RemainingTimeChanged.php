233c331d338600db8835a5f2833a8fbd0f95d0e3
Mahmoud Badr <mahmoud.hussiba@gmail.com>
2023-08-25 20:05:52 +0300

    Creating events and a Command to Broadcast Game Events on Realtime

---
+++ app/Events/RemainingTimeChanged.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Log;
+
+class RemainingTimeChanged implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public $time;
+
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    public function __construct($time)
+    {
+        $this->time = $time;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return \Illuminate\Broadcasting\Channel|array
+     */
+    public function broadcastOn()
+    {
+        \Log::debug("Time: ". $this->time);
+        return new Channel('game');
+    }
+}
